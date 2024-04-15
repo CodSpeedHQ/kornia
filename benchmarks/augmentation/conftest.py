a0158e1e3483a19d89b6eda9673f8a92b19ddf89
Adrien Cacciaguerra <adrien.caccia@gmail.com>
2024-04-15 18:51:11 -0400

    feat: add CodSpeed to the project

--- benchmarks/augmentation/conftest.py
+++ benchmarks/augmentation/conftest.py
@@ -9,8 +9,11 @@ def shape(B, C, H, W):
 
 
 def pytest_generate_tests(metafunc):
-    B = [1, 5]
-    C = [1, 3]
+    # B = [1, 5]
+    # C = [1, 3]
+    # use only 1x1x128x128 for now, to only have a single test case
+    B = [1]
+    C = [1]
     H = W = [128]  # , 237, 512]
     params = list(product(B, C, H, W))
     metafunc.parametrize("B, C, H, W", params)
